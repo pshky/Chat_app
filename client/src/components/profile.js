d7b135b2dcfdd937e6130fe718129fc1902ad7a5
Prasiddha <shakyaprasiddha8@gmail.com>
2023-01-31 22:48:08 +0545

    changed layout for signout

---
+++ client/src/components/profile.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react'
+import { Skeleton } from "antd";
+
+const Profile = ()=>{
+    const [userList, setUserList] = useState([])
+
+    const fetchData = async()=>{
+        const response = await fetch("http://localhost:5000/userlist")
+        const data = await response.json()
+
+        if(data){
+            setUserList(data.userList)
+        }
+    }
+  
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    return(
+        <section>
+            <div className='container'>
+                <div className='userList'>
+                    <h1>Current Users</h1>
+                    {userList.length > 0 ? userList.map(user=>{
+                        return(
+                            <li key={user.id}>{user.name}</li>
+                            )
+                    }): (
+            <Skeleton active paragraph={{ rows: 3 }} />
+            // <Skeleton />
+          )}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Profile
--- client/src/components/sideNav.js
+++ client/src/components/sideNav.js
@@ -1,110 +1,92 @@
 import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-  } from '@ant-design/icons';
-  import { Layout, Menu, theme } from 'antd';
-  import React, { useState } from 'react';
-  import {useNavigate,Link} from "react-router-dom";
-  import { useSelector, useDispatch } from 'react-redux';
-  import {resetDetails} from '../reducers/userSlice'
-  const { Header, Sider, Content } = Layout;
-  const SideNav = () => {
-    const {name} = useSelector(state=> state.user)
-    const [collapsed, setCollapsed] = useState(false);
-    const {
-      token: { colorBgContainer },
-    } = theme.useToken();
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const logout = () => {
-      dispatch(resetDetails())
-      navigate('/')
-  }
-    return (
-      
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-          <div className="logo" />
-          <Menu
-          onClick={({key})=>{
-            if(key === "logout"){
-              // 
-            }else{
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+import React, { useState } from 'react';
+import { useNavigate, Link } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { resetDetails } from '../reducers/userSlice'
+const { Header, Sider, Content } = Layout;
+const SideNav = () => {
+  const { name } = useSelector(state => state.user)
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  return (
+
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu
+          onClick={({ key }) => {
+            if (key === "logout") {
+              dispatch(resetDetails())
+              navigate('/')
+            } else {
               navigate(key)
             }
           }}
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            items={[
-              {
-                key: '/profile',
-                icon: <UserOutlined />,
-                label: 'My Profile',
-              },
-              {
-                key: '/userlist',
-                icon: <VideoCameraOutlined />,
-                label: 'Fellow Users',
-              },
-              {
-                key: '/messages',
-                icon: <UploadOutlined />,
-                label: 'My Messages',
-              },
-              {
-                key: 'logout',
-                icon: <UploadOutlined />,
-                label: <button onClick={() => logout()}>Logout</button>,
-              },
-            ]
-            }
-          />
-        </Sider>
-        <Layout className="site-layout">
-          <Header
-            style={{
-              padding: 0,
-              background: colorBgContainer,
-            }}
-          >
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: () => setCollapsed(!collapsed),
-            })}
-          </Header>
-          <Content
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              minHeight: 280,
-              background: colorBgContainer,
-            }}
-          >
-            <h4>Welcome, {name}</h4>
-          </Content>
-        </Layout>
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '/profile',
+              icon: <UserOutlined />,
+              label: 'My Profile',
+            },
+            {
+              key: '/userlist',
+              icon: <VideoCameraOutlined />,
+              label: 'Fellow Users',
+            },
+            {
+              key: '/messages',
+              icon: <UploadOutlined />,
+              label: 'My Messages',
+            },
+            {
+              key: 'logout',
+              icon: <UploadOutlined />,
+              label: 'Logout',
+            },
+          ]
+          }
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <h4>Welcome, {name}</h4>
+        </Content>
       </Layout>
-    );
-  };
-  export default SideNav;
-//   #components-layout-demo-custom-trigger .trigger {
-//     padding: 0 24px;
-//     font-size: 18px;
-//     line-height: 64px;
-//     cursor: pointer;
-//     transition: color 0.3s;
-//   }
-  
-//   #components-layout-demo-custom-trigger .trigger:hover {
-//     color: #1890ff;
-//   }
-  
-//   #components-layout-demo-custom-trigger .logo {
-//     height: 32px;
-//     margin: 16px;
-//     background: rgba(255, 255, 255, 0.3);
-//   }
+    </Layout>
+  );
+};
+export default SideNav;
+
+
