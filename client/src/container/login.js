2a390e51767f8d85be9225448f3784b08bc48b68
Prasiddha <shakyaprasiddha8@gmail.com>
2023-02-20 18:27:08 +0545

    added css to login page

--- client/src/container/login.js
+++ client/src/container/login.js
@@ -1,76 +1,163 @@
-import React from 'react';
- import { Formik, Form, Field } from 'formik';
- import * as Yup from 'yup';
- import { message } from 'antd';
- import ShowhidePassword from "../components/showHidePassword";
- import { useDispatch, useSelector } from "react-redux"
-import {setUserDetails}  from "../reducers/userSlice"
- import { useNavigate, Link, Route, Routes } from 'react-router-dom';
-const Login = ()=>{
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const loginUser = async(values, resetForm)=>{
-      const requestOptions = {
-          method: "POST",
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(values)
-      };
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { loginRoute } from "../utils/APIRoutes";
 
-      const response = await fetch('http://localhost:5000/login', requestOptions);
-      const data = await response.json()
-      console.log(data)
-      if(data.msg === 'login success'){
-          dispatch(setUserDetails(data.userDetails))
-          message.success(data.msg)
-          navigate("/dashBoard")
-      }else{
-          message.error(data.msg)
+export default function Login() {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({ username: "", password: "" });
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  useEffect(() => {
+    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/");
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const validateForm = () => {
+    const { username, password } = values;
+    if (username === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    } else if (password === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      const { username, password } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
       }
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+
+        navigate("/");
+      }
+    }
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+            min="3"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Log In</button>
+          <span>
+            Don't have an account ? <Link to="/register">Create One.</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
+    </>
+  );
+}
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
   }
 
- const UserSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Required'),
-   password: Yup.string()
-     .min(2, 'Too Short!')
-     .max(50, 'Too Long!')
-     .required('Required'),
-   
- });
-
- return(
-      
-  <>
-    <div className='container'>
-      <div className='form'>
-     <h1>Login</h1>
-     
-     <Formik
-       initialValues={{
-         email: '',
-         password: '',
-       }}
-       
-       validationSchema={UserSchema}
-       onSubmit={(values, { resetForm })=>{
-          loginUser(values)
-       }}
-     >
-       {({ errors, touched , values, handleChange, handleBlur, handleSubmit}) => (
-         <Form>
-         <Field name="email" type="email" placeholder="Enter your Email"/>
-           {errors.email && touched.email ? <div>{errors.email}</div> : null}<br/>
-         <Field name="password" placeholder="Enter your password" value={values.password} component={ShowhidePassword} onChange={handleChange} onBlur={handleBlur}/>
-           {errors.password && touched.password ? (
-             <div>{errors.password}</div>
-           ) : null}<br/>
-           <button type="submit">Submit</button>
-         </Form>
-       )}
-     </Formik>
-     <p style={{ marginTop: '10px' }}>Dont have an account? <Link to="/signup">Signup</Link> here</p>
-     </div>
-     </div>
-     </>
- );
-}
- export default Login;
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
