18954ca37b5b649a5d895a47a7d2836c647814d1
Prasiddha <shakyaprasiddha8@gmail.com>
2023-02-20 18:25:59 +0545

    added socket to chat.js

--- client/src/container/chat.js
+++ client/src/container/chat.js
@@ -1,54 +1,83 @@
-import React from "react";
-// import SideBar from "../../component/SideBar/sideBar";
-import ChatBox from "../components/chatBox";
-import WelcomeBox from "../components/welcomeBox";
-import { useState, useEffect } from "react";
-// import "./chat.css";
-import { useSelector } from "react-redux";
-// import NavBar from "../../component/Navbar/navBar";
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+import styled from "styled-components";
+import { allUsersRoute, host } from "../utils/APIRoutes";
+import ChatContainer from "../components/ChatContainer";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
 
-const Chat = () => {
-  const { name } = useSelector((state) => state.user);
-  const [userList, setUserList] = useState([]);
-  const [selectedUser, setSelectedUser] = useState(null);
-
-  const handleChatChange = (chat) => {
-    setSelectedUser(chat);
-  };
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_BASE_URL}/register`);
-    const data = await response.json();
-    // console.log(data);
-
-    if (data) {
-      setUserList(data.usersList);
+export default function Chat() {
+  const navigate = useNavigate();
+  const socket = useRef();
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(async () => {
+    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/login");
+    } else {
+      setCurrentUser(
+        await JSON.parse(
+          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+        )
+      );
     }
-  };
-
-  useEffect(() => {
-    fetchUsers();
   }, []);
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
 
+  useEffect(async () => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+      } else {
+        navigate("/setAvatar");
+      }
+    }
+  }, [currentUser]);
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
   return (
-    <div className="chat">
-      {/* <div>
-        <NavBar />
-      </div> */}
-      <div className="chat-body">
-        
-          {userList}
-          {selectedUser}
-          {setSelectedUser}
-          {handleChatChange}
-        
-        {selectedUser ? (
-          <ChatBox selectedUser={selectedUser} />
-        ) : (
-          <WelcomeBox />
-        )}
-      </div>
-    </div>
+    <>
+      <Container>
+        <div className="container">
+          <Contacts contacts={contacts} changeChat={handleChatChange} />
+          {currentChat === undefined ? (
+            <Welcome />
+          ) : (
+            <ChatContainer currentChat={currentChat} socket={socket} />
+          )}
+        </div>
+      </Container>
+    </>
   );
-};
-export default Chat;
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
